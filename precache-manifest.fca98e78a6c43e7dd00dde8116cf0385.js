df11ba1417900137b989e4a516c29ac430c0b78f
Tom Dunlap <tom@motevets.com>
2020-08-19 12:18:36 -0400

    Updates

---
+++ precache-manifest.fca98e78a6c43e7dd00dde8116cf0385.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "894e505b1a569f67d973ed3d7f3e6e79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c906852979aca5ceb6ff",
+    "url": "/static/js/2.245f8f71.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/static/js/2.245f8f71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18f4160272e7e0a0c866",
+    "url": "/static/js/main.3de51a6d.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  }
+]);
